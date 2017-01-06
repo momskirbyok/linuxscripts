5efe7d6bb573985cf31175e85b8356d47a1321a4
momskirbyok <nathanringraham@gmail.com>
2017-01-06 17:14:28 -0600

    Update updatePihole.sh

--- updatePihole.sh
+++ updatePihole.sh
@@ -1,11 +1,11 @@
 cd ~/Desktop
 
-nano piHoleUpdate.log
+touch piHoleUpdate.log 
 
 echo > piHoleUpdate.log
 echo PiHole Domain update ran at: > piHoleUpdate.log
 date >> piHoleUpdate.log
-pihole -g >> piHoleUpdate.log
+pihole -g >> piHoleUpdate.log #I tried to append the actual progress to the Desktop file, but it refused to do so.. 
 
 
 
