bbcc5e6918a8a64405da6fc25a44d1346e8d8058
momskirbyok <nathanringraham@gmail.com>
2017-01-09 12:06:17 -0600

    Added comments to the script

--- updatePihole.sh
+++ updatePihole.sh
@@ -1,10 +1,18 @@
-cd ~/Desktop
 
+#change directory to desktop
+cd ~/Desktop
+#makes log file to write to
 touch piHoleUpdate.log
 
+# erases the file to ensure this file doesn't go on forever for each time it updates
 echo > piHoleUpdate.log
+
+# these two lines print the date at the top of the log file 
+
 echo PiHole Domain update ran at: > piHoleUpdate.log
 date >> piHoleUpdate.log
+
+#writes the contents of the pihole update to the log file
 pihole -g >> piHoleUpdate.log #I tried to append the actual progress to the Desktop file, but it refused to do so.. 
 
 
